f5d968d31c35a84abfdb0ed1d2380df8729c849a
leon0128 <leon0128cocoa@gmail.com>
2019-09-03 21:02:38 +0900

    [update] Creating a basic flow framework for the game

--- main/game.cpp
+++ main/game.cpp
@@ -1,5 +1,42 @@
 #include "game.hpp"
 
-Game::Game()
+Game::Game():
+    mIsLoop(true)
 {
+}
+
+bool Game::initialize()
+{
+
+    return true;
+}
+
+void Game::runLoop()
+{
+    while(mIsLoop)
+    {
+        input();
+        update();
+        output();
+    }
+}
+
+void Game::finalize()
+{
+    
+}
+
+void Game::input()
+{
+
+}
+
+void Game::update()
+{
+
+}
+
+void Game::output()
+{
+
 }
--- main/game.hpp
+++ main/game.hpp
@@ -1,8 +1,27 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+
 class Game
 {
 public:
     Game();
     ~Game(){}
+
+    // main で呼び出す
+    bool initialize();
+    void runLoop();
+    void finalize();
+
+    // Game 終了時に呼び出す
+    void end(){mIsLoop = false;}
+
+private:
+    // runLoop() 内のループに使用
+    void input();
+    void update();
+    void output();
+
+    // フラグ
+    bool mIsLoop; // ゲームを続けるか
 };
