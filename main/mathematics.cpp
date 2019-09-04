2b32dab776a999e638a20d0ae55e02356dd9d8e8
leon0128 <leon0128cocoa@gmail.com>
2019-09-04 14:33:08 +0900

    [update] Implementation of Vector2 class

--- main/mathematics.cpp
+++ main/mathematics.cpp
@@ -1,5 +1,11 @@
 #include "mathematics.hpp"
 
+const Vector2 Vector2::ZERO(0.0f, 0.0f);
+const Vector2 Vector2::UNIT_X(1.0f, 0.0f);
+const Vector2 Vector2::UNIT_Y(0.0f, 1.0f);
+const Vector2 Vector2::NEGATIVE_UNIT_X(-1.0f, 0.0f);
+const Vector2 Vector2::NEGATIVE_UNIT_Y(0.0f, -1.0f);
+
 Coordinate2::Coordinate2():
     x(0),
     y(0)
@@ -16,4 +22,122 @@ void Coordinate2::set(int inX, int inY)
 {
     x = inX;
     y = inY;
+}
+
+Vector2::Vector2():
+    x(0.0f),
+    y(0.0f)
+{
+}
+
+Vector2::Vector2(float inX, float inY):
+    x(inX),
+    y(inY)
+{
+}
+
+void Vector2::set(float inX, float inY)
+{
+    x = inX;
+    y = inY;
+}
+
+const float* Vector2::getAsFloatPointer() const
+{
+    return reinterpret_cast<const float*>(&x);
+}
+
+float Vector2::lengthSquared() const
+{
+    float len = x * x + y * y;
+    return len;
+}
+
+float Vector2::length() const
+{
+    float len = std::sqrt(x * x + y * y);
+    return len;
+}
+
+void Vector2::normalize()
+{
+    float len = length();
+    x /= len;
+    y /= len;
+}
+
+Vector2 Vector2::normalize(const Vector2& inVector)
+{
+    Vector2 outVector = inVector;
+    outVector.normalize();
+    return outVector;
+}
+
+float Vector2::dot(const Vector2& lhs,
+                   const Vector2& rhs)
+{
+    float out = lhs.x * rhs.x +
+                lhs.y * rhs.y;
+    return out;
+}
+
+Vector2 operator+(const Vector2& lhs,
+                  const Vector2& rhs)
+{
+    Vector2 vector(lhs.x + rhs.x,
+                   lhs.y + rhs.y);
+    return vector;
+}
+
+Vector2 operator-(const Vector2& lhs,
+                  const Vector2& rhs)
+{
+    Vector2 vector(lhs.x - rhs.x,
+                   lhs.y - rhs.y);
+    return vector;
+}
+
+Vector2 operator*(const Vector2& lhs,
+                  const Vector2& rhs)
+{
+    Vector2 vector(lhs.x * rhs.x,
+                   lhs.y * rhs.y);
+    return vector;
+}
+
+Vector2 operator*(const Vector2& inVector,
+                  float scalar)
+{
+    Vector2 outVector(inVector.x * scalar,
+                      inVector.y * scalar);
+    return outVector;
+}
+
+Vector2 operator*(float scalar,
+                  const Vector2& inVector)
+{
+    Vector2 outVector(inVector.x * scalar,
+                      inVector.y * scalar);
+    return outVector;
+}
+
+Vector2& Vector2::operator+=(const Vector2& rhs)
+{
+    x += rhs.x;
+    y += rhs.y;
+    return *this;
+}
+
+Vector2& Vector2::operator-=(const Vector2& rhs)
+{
+    x -= rhs.x;
+    y -= rhs.y;
+    return *this;
+}
+
+Vector2& Vector2::operator*=(float scalar)
+{
+    x *= scalar;
+    y *= scalar;
+    return *this;
 }
--- main/mathematics.hpp
+++ main/mathematics.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 // 2次元の整数値座標
 class Coordinate2
 {
@@ -9,8 +11,59 @@ public:
 
     // コンストラクタ
     Coordinate2();
-    explicit Coordinate2(int inX, int inY);
+    Coordinate2(int inX, int inY);
 
     // 値の設定
     void set(int inX, int inY);
+};
+
+// 2次元ベクトル
+class Vector2
+{
+public:
+    float x; // X軸
+    float y; // Y軸
+    
+    // 定数
+    static const Vector2 ZERO;
+    static const Vector2 UNIT_X;
+    static const Vector2 UNIT_Y;
+    static const Vector2 NEGATIVE_UNIT_X;
+    static const Vector2 NEGATIVE_UNIT_Y;
+
+    // コンストラクタ
+    Vector2();
+    Vector2(float inX, float inY);
+
+    // 値の設定
+    void set(float inX, float inY);
+    // Vector2をfloat*にキャスト(openGLで使用)
+    const float* getAsFloatPointer() const;
+    // 長さの2乗
+    float lengthSquared() const;
+    // 長さ
+    float length() const;
+    // 正規化
+    void normalize();
+    
+    // 正規化
+    static Vector2 normalize(const Vector2& inVector);
+    // ドット積
+    static float dot(const Vector2& lhs,
+                     const Vector2& rhs);
+
+    // 演算
+    friend Vector2 operator+(const Vector2& lhs,
+                             const Vector2& rhs);
+    friend Vector2 operator-(const Vector2& lhs,
+                             const Vector2& rhs);
+    friend Vector2 operator*(const Vector2& lhs,
+                             const Vector2& rhs);
+    friend Vector2 operator*(const Vector2& inVector,
+                             float scalar);
+    friend Vector2 operator*(float scalar,
+                             const Vector2& inVector);
+    Vector2& operator+=(const Vector2& rhs);
+    Vector2& operator-=(const Vector2& rhs);
+    Vector2& operator*=(float scalar);
 };
