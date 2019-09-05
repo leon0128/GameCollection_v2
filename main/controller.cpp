7fb14a7a8bdc5b002c184d98cb04f62c788e65ed
leon0128 <leon0128cocoa@gmail.com>
2019-09-05 21:59:40 +0900

    [update] Implementation of Controller::addActor()

--- main/controller.cpp
+++ main/controller.cpp
@@ -1,8 +1,13 @@
 #include "controller.hpp"
 #include "game.hpp"
+#include "actor.hpp"
 
 Controller::Controller(Game* game):
-    mGame(game)
+    mActors(),
+    mPendingActors(),
+    mGame(game),
+    mLastTicks(0),
+    mIsUpdatingActor(false)
 {
 }
 
@@ -14,4 +19,13 @@ bool Controller::initialize()
 void Controller::update()
 {
 
+}
+
+void Controller::addActor(Actor* actor)
+{
+    // mActors が更新中かどうかで格納先の変更
+    if(!mIsUpdatingActor)
+        mActors.emplace_back(actor);
+    else
+        mPendingActors.emplace_back(actor);
 }
--- main/controller.hpp
+++ main/controller.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <SDL2/SDL.h>
+#include <vector>
+
 class Controller
 {
 public:
@@ -8,8 +11,19 @@ public:
 
     bool initialize();
 
+    // Game::update() で呼び出す
     void update();
 
+    // Actor クラスのコンストラクタで呼び出す
+    void addActor(class Actor* actor);
+    void removeActor(class Actor* actor);
+
 private:
+    std::vector<class Actor*> mActors;        // Actor
+    std::vector<class Actor*> mPendingActors; // Actor更新中に一時的に格納される
+
     class Game* mGame; // 自身が所属するGameクラス
+    Uint32 mLastTicks; // 最終更新時間
+
+    bool mIsUpdatingActor; // Actorを更新中かどうか
 };
