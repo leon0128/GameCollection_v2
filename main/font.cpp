5796d0592dc441d0fa6999a22ef4bcd3f4d92033
leon0128 <leon0128cocoa@gmail.com>
2019-09-08 12:52:48 +0900

    [update] Implementaion of Font::initialize()

---
+++ main/font.cpp
@@ -0,0 +1,25 @@
+#include "font.hpp"
+
+Font::Font():
+    mFontMap()
+{
+}
+
+bool Font::initialize()
+{
+    // SDL_TTF の初期化
+    TTF_Init();
+
+    char filename[] = "font/Cica-Bold.ttf";
+    ESize sizes[] = {SIZE_8, SIZE_12, SIZE_16, SIZE_20,
+                     SIZE_30, SIZE_40, SIZE_50, SIZE_60,
+                     SIZE_80, SIZE_100};
+    for(ESize size : sizes)
+    {
+        mFontMap.emplace(size,
+                         TTF_OpenFont(filename,
+                                      size));
+    }
+
+    return true;
+}
---
+++ main/font.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <SDL2/SDL_ttf.h>
+#include <unordered_map>
+
+class Font
+{
+public:
+
+    // フォントの大きさ
+    enum ESize
+    {
+        SIZE_8  = 8,
+        SIZE_12 = 12,
+        SIZE_16 = 16,
+        SIZE_20 = 20,
+        SIZE_30 = 30,
+        SIZE_40 = 40,
+        SIZE_50 = 50,
+        SIZE_60 = 60,
+        SIZE_80 = 80,
+        SIZE_100 = 100
+    };
+
+    Font();
+    ~Font(){}
+
+    bool initialize();
+    void finalize();
+
+    SDL_Surface* getCharSurface(char character,
+                                ESize size);
+
+private:
+    std::unordered_map<ESize, TTF_Font*> mFontMap; // 作成したフォントのMap
+};
