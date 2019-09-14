f02e3fa3444e30fe60188c8078775189ca8c839b
leon0128 <leon0128cocoa@gmail.com>
2019-09-14 21:04:20 +0900

    [fix] Change the imported font

--- main/font.cpp
+++ main/font.cpp
@@ -10,7 +10,7 @@ bool Font::initialize()
     // SDL_TTF の初期化
     TTF_Init();
 
-    char filename[] = "font/Cica-Bold.ttf";
+    char filename[] = "font/Hack-Regular.ttf";
     ESize sizes[] = {SIZE_8, SIZE_12, SIZE_16, SIZE_20,
                      SIZE_30, SIZE_40, SIZE_50, SIZE_60,
                      SIZE_80, SIZE_100};
